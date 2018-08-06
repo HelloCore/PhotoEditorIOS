6f23d9e455007858c524e4836a801fe0b4f3049c
Core <aukwat@gmail.com>
2018-08-06 15:54:34 +0700

    Initial Commit

---
+++ PhotoEditorSDK/PhotoEditorManager.swift
@@ -0,0 +1,9 @@
+//
+//  PhotoEditorManager.swift
+//  PhotoEditorSDK
+//
+//  Created by Akkharawat Chayapiwat on 8/6/18.
+//  Copyright © 2018 Akkharawat Chayapiwat. All rights reserved.
+//
+
+import Foundation
---
+++ PhotoEditorSDK/PhotoEditorSDK.h
@@ -0,0 +1,19 @@
+//
+//  PhotoEditorSDK.h
+//  PhotoEditorSDK
+//
+//  Created by Akkharawat Chayapiwat on 8/6/18.
+//  Copyright © 2018 Akkharawat Chayapiwat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PhotoEditorSDK.
+FOUNDATION_EXPORT double PhotoEditorSDKVersionNumber;
+
+//! Project version string for PhotoEditorSDK.
+FOUNDATION_EXPORT const unsigned char PhotoEditorSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PhotoEditorSDK/PublicHeader.h>
+
+
